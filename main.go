562298d809728f5d7920155452056bc1117a915d
alifoo <alissonayres.trb@gmail.com>
2025-04-15 01:14:40 -0300

    finally added stateful handlers

--- main.go
+++ main.go
@@ -4,26 +4,55 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
 
-func ReadinessEndpoint(h http.ResponseWriter, req *http.Request) {
-	h.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	h.WriteHeader(http.StatusOK)
+func ReadinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 
-	h.Write([]byte("OK"))
+	w.Write([]byte("OK"))
 
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { // handlerfunc turns any func with the right signature into a http handler
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsReader(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics reset to 0"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
 	mux.HandleFunc("/healthz", ReadinessEndpoint)
-	staticPath := http.Dir("./")
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(staticPath)))
+	apiCfg := apiConfig{}
+	handler := http.StripPrefix("/app/", http.FileServer(http.Dir("./")))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/metrics", apiCfg.metricsReader)
+	mux.HandleFunc("/reset", apiCfg.resetMetrics)
 
 	err := server.ListenAndServe()
 	if err != nil {
